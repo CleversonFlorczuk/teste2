bec45cc2841eef204731fc078a379c172c0838b9
Cleverson Florczuk <rikudoorinne@gmail.com>
2022-03-26 22:22:19 -0300

    Start Project

---
+++ form.php
@@ -0,0 +1,2 @@
+<?php
+echo "estou no form";
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="aqui.css" media="screen" />
+</head>
+<body>
+	<h1>Teste</h1>
+	<p>Cleverson</p>
+	<p>S2</p>
+	<p>Evandro</p>
+	<hr>
+	<form action="form.php">
+		<input type="text" placeholder="Coloque uma frase de amor aqui!">
+		<button>Confirme o nosso amor</button>
+	</form>
+</body>
+</html>
